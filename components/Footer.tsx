cb6876fd3e82a779bcea43e0f819cc4386ac8a49
Venclise <zunnurainzahoor955@gmail.com>
2026-02-27 10:22:16 +0500

    Added some changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,9 @@ import React from 'react'
 export default function Footer() {
     let date = new Date().getFullYear()
   return (
-    <div id="faqs" className='bg-black lg:p-10 p-5 flex lg:flex-row flex-col gap-4 w-full items-center justify-between'> 
+    <div id="faqs" className='bg-black lg:p-10 p-5 flex  flex-col gap-8  w-full items-center justify-between'> 
+    <div className='flex items-center lg:flex-row flex-col lg:justify-between w-full gap-8'>
+
     <div className='flex flex-col gap-4'>
       <Link href="/" className='w-max font-semibold text-2xl text-red-400'>
       Spherical
@@ -15,22 +17,29 @@ export default function Footer() {
       </p>
     </div>
     <div>
-        <span className='text-red-400'>
+        <span className='text-red-400 text-center'>
         Quick Links.
         </span>
+        <div className='flex w-full lg:flex-col flex-row '>
+
         
                 {
-        Links.map(({id,link,title}) => (
+                  Links.map(({id,link,title}) => (
+                    
+                    <Link href={link}  key={id} className=" lg:text-left text-center  transition-all text-white">
             
-            <Link href={link}  key={id} className="w-full    transition-all text-white">
-            
-                <p className=" text-lg transition-all text-gray-300 hover:underline capitalize">{title}.</p>
+                <p className=" text-md   transition-all text-gray-300 hover:underline capitalize">{title}.</p>
           
                </Link>
         ))
       }
+      </div>
+      </div>
         
     </div>
+    <p className='text-gray-200'>
+    Created by <Link className='text-blue-400 underline' href="https://weblify-nu.vercel.app/">Weblify.</Link>
+    </p>
     </div>
   )
 }
